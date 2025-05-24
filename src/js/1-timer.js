76ee1a990459808a72f9b578aa461d3e082b2c07
OlaGrikun <olagrikun@gmail.com>
2025-05-24 10:19:44 +0300

    created files

---
+++ src/js/1-timer.js
@@ -0,0 +1,109 @@
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+const input = document.querySelector("#datetime-picker");
+
+let userSelectedDate;
+let intervalId;
+
+const btn = document.querySelector('[data-start]');
+btn.setAttribute('disabled', true);
+
+
+
+const timerElements = {
+  days: document.querySelector('[data-days]'),
+  hours: document.querySelector('[data-hours]'),
+  minutes: document.querySelector('[data-minutes]'),
+  seconds: document.querySelector('[data-seconds]'),
+};
+
+
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+      ;
+
+      if (Date.now() < selectedDates[0]) {
+          userSelectedDate = selectedDates[0];
+          btn.removeAttribute('disabled');
+      }
+      else {
+          iziToast.info({
+            title: 'Error',
+            message: 'Please choose a date in the future',
+            position: 'topRight' 
+});
+       
+      }
+  },
+};
+flatpickr(input, options);
+
+
+
+
+function addLeadingZero(ms) {
+    const { days, hours, minutes, seconds } = convertMs(ms);
+    let d = days.toString().padStart(2, '0');
+    let h = hours.toString().padStart(2, '0');
+    let m = minutes.toString().padStart(2, '0');
+    let s = seconds.toString().padStart(2, '0');
+return {d, h, m, s}
+}
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+
+
+btn.addEventListener('click', onClick);
+function onClick() {
+    //const initDate = ;
+    intervalId = setInterval(() => {
+        const currentTime = Date.now();
+        const diffMs = userSelectedDate - currentTime;
+        const timeStr = addLeadingZero(diffMs);
+
+        timerElements.days.textContent = timeStr.d;
+        timerElements.hours.textContent = timeStr.h;
+        timerElements.minutes.textContent = timeStr.m;
+        timerElements.seconds.textContent = timeStr.s;
+
+        btn.setAttribute('disabled', true); 
+
+        if (diffMs < 1000) {
+            clearInterval(intervalId);
+            btn.removeAttribute('disabled'); 
+        }
+   
+    }, 1000)
+    
+
+}
+
+
+
+
